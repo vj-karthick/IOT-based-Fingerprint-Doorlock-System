a0d2333a18fa9747f7cd95a23509078fb8fa0ec7
M Karthick <132045489+vj-karthick@users.noreply.github.com>
2024-01-21 11:16:09 +0530

    v1

---
+++ Source Code.ino
@@ -0,0 +1,26 @@
+int value1;
+#define relay 9
+const int buzzer = 7; 
+void setup() { 
+Serial.begin(9600); 
+pinMode(relay, OUTPUT); 
+pinMode(buzzer, OUTPUT); 
+digitalWrite(relay, LOW); 
+} 
+void loop() { 
+Serial.print("Reading"); 
+while(Serial.available()==0); 
+value1 = Serial.read(); 
+Serial.print(value1); 
+if (value1==1) {
+ Serial.print("Unlocking"); 
+digitalWrite(relay, LOW); 
+digitalWrite(buzzer, HIGH); 
+delay(1000); 
+digitalWrite(buzzer, LOW); 
+} 
+if (value1==0) { 
+digitalWrite(relay, HIGH); 
+Serial.print("Locking"); 
+} 
+}
